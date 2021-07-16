a01cec0475ca0d1a8f3b3646a890c5346c54ea66
MrManu <superbrandon2018@gmail.com>
2021-07-15 23:35:02 -0500

    Text Editor Working

--- components/TextEditor.js
+++ components/TextEditor.js
@@ -1,7 +1,22 @@
+import dynamic from 'next/dynamic'
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
+const Editor = dynamic(() => import('react-draft-wysiwyg').then(module => module.Editor),
+    {
+        ssr: false,
+    }
+    )
+
 function TextEditor() {
+
+    
     return (
-        <div>
-            
+        <div className='bg-[#F8F9FA] min-h-screen pb-6'>
+            <Editor
+                // editorState={editorState}
+                toolbarClassName='flex sticky top-0 z-50 !justify-center mx-auto'
+                editorClassName='mt-6 bg-white shadow-lg max-w-4xl mx-auto mb-12 border p-8'
+            ></Editor>
         </div>
     )
 }
